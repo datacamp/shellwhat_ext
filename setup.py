b4be82e128fcdf6a5e4fa22e0bb5576c40512d89
Greg Wilson <greg@datacamp.com>
2017-10-10 11:23:52 -0400

    Building a simple testing extension for the shell

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='shellwhat_ext',
+      version='0.1',
+      description='Extensions to shellwhat testing',
+      url='https://github.com/gvwilson/shellwhat_ext',
+      author='Greg Wilson',
+      author_email='greg@datacamp.com',
+      license='MIT',
+      packages=['shellwhat_ext'],
+      zip_safe=False)
