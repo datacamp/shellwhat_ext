8a1cb7e181ec4ab2675c9a5b4414afbc20a94f95
Greg Wilson <greg@datacamp.com>
2017-10-10 13:31:25 -0400

    Line-by-line file comparison

--- shellwhat_ext/__init__.py
+++ shellwhat_ext/__init__.py
@@ -1,2 +1,27 @@
-def test_file_compare():
-    return False
+def test_lines_compare(leftFilename, rightFilename):
+    '''Return empty string if two text files are line-by-line equal
+    (ignoring whitespace) and an error message if they are not.'''
+
+    try:
+        with open(leftFilename, 'r') as stream:
+            leftList = stream.readlines()
+        with open(rightFilename, 'r') as stream:
+            rightList = stream.readlines()
+    except Exception as err:
+        return str(err)
+
+    if len(leftList) != len(rightList):
+        return 'Files have different lengths'
+
+    leftList = [x.strip() for x in leftList]
+    rightList = [x.strip() for x in rightList]
+
+    diffs = []
+    for (i, leftLine, rightLine) in zip(range(len(leftList)), leftList, rightList):
+        if leftLine != rightLine:
+            diffs.append(i+1)
+
+    if diffs:
+        return 'Line(s) differ: {}'.format(', '.join([str(x) for x in diffs]))
+
+    return '' # all good
