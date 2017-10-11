338814a80f88ef1f9af724fe6afdf580b6138911
Greg Wilson <greg@datacamp.com>
2017-10-11 19:31:53 -0400

    Debugging message format

--- shellwhat_ext/__init__.py
+++ shellwhat_ext/__init__.py
@@ -31,7 +31,7 @@ def test_compare_file_to_file(state, actualFilename, expectFilename, debug=None)
         msg = 'File {} has wrong length: got {} expected {}'
         msg = msg.format(actualFilename, actualLen, expectLen)
         if debug is not None:
-            msg += ' <| {} |>'.format(debug)
+            msg += ' (-( {} )-)'.format(debug)
         state.do_test(msg)
 
     diffs = []
@@ -43,7 +43,7 @@ def test_compare_file_to_file(state, actualFilename, expectFilename, debug=None)
         msg = 'Line(s) in {} not as expected: {}'
         msg = msg.format(actualFilename, ', '.join([str(x) for x in diffs]))
         if debug is not None:
-            msg += ' <| {} // expect {} // actual {} |>'.format(debug, str(expectList), str(actualList))
+            msg += ' (-( {} // expect {} // actual {} )-)'.format(debug, str(expectList), str(actualList))
         state.do_test(msg)
 
     return state # all good
@@ -56,7 +56,7 @@ def test_file_perms(state, path, perms, message, debug=None):
     if not os.path.exists(path):
         msg = '{} does not exist'.format(path)
         if debug is not None:
-            msg += ' <| {} |>'.format(debug)
+            msg += ' (-( {} )-)'.format(debug)
         state.do_test(msg)
     controls = {'r' : os.R_OK, 'w' : os.W_OK, 'x' : os.X_OK}
     flags = 0
