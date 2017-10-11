82d5d0ae8a49a41141f123e888d8ca8ee11716a8
Greg Wilson <greg@datacamp.com>
2017-10-11 19:17:52 -0400

    More debugging info

--- shellwhat_ext/__init__.py
+++ shellwhat_ext/__init__.py
@@ -31,7 +31,7 @@ def test_compare_file_to_file(state, actualFilename, expectFilename, debug=None)
         msg = 'File {} has wrong length: got {} expected {}'
         msg = msg.format(actualFilename, actualLen, expectLen)
         if debug is not None:
-            msg += ' [' + debug + ']'
+            msg += ' <| {} |>'.format(debug)
         state.do_test(msg)
 
     diffs = []
@@ -43,20 +43,21 @@ def test_compare_file_to_file(state, actualFilename, expectFilename, debug=None)
         msg = 'Line(s) in {} not as expected: {}'
         msg = msg.format(actualFilename, ', '.join([str(x) for x in diffs]))
         if debug is not None:
-            msg += ' [' + debug + ']'
-            msg += ' expect: {}'.format(str(expectList))
-            msg += ' actual: {}'.format(str(actualList))
+            msg += ' <| {} // expect {} // actual {} |>'.format(debug, str(expectList), str(actualList))
         state.do_test(msg)
 
     return state # all good
 
 
 @state_dec
-def test_file_perms(state, path, perms, message):
+def test_file_perms(state, path, perms, message, debug=None):
     '''Test that something has the required permissions.'''
 
     if not os.path.exists(path):
-        state.do_test('{} does not exist'.format(path))
+        msg = '{} does not exist'.format(path)
+        if debug is not None:
+            msg += ' <| {} |>'.format(debug)
+        state.do_test(msg)
     controls = {'r' : os.R_OK, 'w' : os.W_OK, 'x' : os.X_OK}
     flags = 0
     for p in perms:
